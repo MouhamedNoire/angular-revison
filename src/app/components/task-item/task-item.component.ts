7d1ea0cd12b921ae99803663f3bab4d68823fea4
mouhamedNoire <mouhamedabdallah.bathilly@uvs.edu.sn>
2023-04-11 15:22:23 +0200

    ajout api , bouton delete , et double click

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, Output , EventEmitter} from '@angular/core';
 import { Task } from 'src/app/Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 @Component({
@@ -7,11 +7,24 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./task-item.component.css']
 })
 export class TaskItemComponent implements OnInit{
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
   // @ts-ignore
   @Input() task : Task;
+
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
   faTimes = faTimes;
   constructor() {
   }
   ngOnInit() {
+  }
+  onDelete(task :Task){
+    this.onDeleteTask.emit(task);
+    /** un event emitter permet de detecter un evenement et de capter l evevenement
+     dans ce cas ci elle capte le task et l envoie dans la classe mere d ou le output ainsi dans
+     la classe mere on utilise le onDeleteTask() et on lui passe la fonction deleteTask()
+     de la classe mere avec comme parametre le task capte par l event emitter .**/
+  }
+  onToggle(task : Task){
+    this.onToggleReminder.emit(task);
   }
 }
