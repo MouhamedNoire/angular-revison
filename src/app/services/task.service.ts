fadca1aa0724c68c76c98e088f7938fb08b056ca
mouhamedNoire <mouhamedabdallah.bathilly@uvs.edu.sn>
2023-04-17 17:12:02 +0200

    ajout add task

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -29,4 +29,9 @@ export class TaskService {
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.put<Task>(url, task , httpOptions )
   }
+
+  addTask(task : Task) : Observable<Task>{
+    // @ts-ignore
+    return this.http.post<Task>(this.apiUrl, task , httpOptions);
+  }
 }
